5c31816cfd1c7f58514849696ea2f701b943a735
MayankSaxena2214 <148477562+MayankSaxena2214@users.noreply.github.com>
2024-02-14 22:09:46 +0530

    Time: 136 ms (90.59%) | Memory: 137 MB (18.51%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> ans;
+        
         vector<int> pos,neg;
         for(auto i:nums){
             if(i>0){
@@ -11,17 +11,26 @@ public:
                 neg.push_back(i);
             }
         }
-        int i=0,j=0;
-        while(i<pos.size() && j<neg.size()){
-            ans.push_back(pos[i++]);
-            ans.push_back(neg[j++]);
+        // int i=0,j=0;
+        // while(i<pos.size() && j<neg.size()){
+        //     ans.push_back(pos[i++]);
+        //     ans.push_back(neg[j++]);
+        // }
+        // while(i<pos.size()){
+        //     ans.push_back(pos[i++]);
+        // }
+        // while(j<neg.size()){
+        //     ans.push_back(neg[j++]);
+        // }
+        int j=0,k=0;
+        for(int i=0;i<nums.size();i++){
+            if(i%2==0){
+               nums[i]=pos[j++];
+            }
+            else{
+                nums[i]=neg[k++];
+            }
         }
-        while(i<pos.size()){
-            ans.push_back(pos[i++]);
-        }
-        while(j<neg.size()){
-            ans.push_back(neg[j++]);
-        }
-        return ans;
+        return nums;
     }
 };
