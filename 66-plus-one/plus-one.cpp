a91735896feaed4aaa1989ac0aa18b909254472c
MayankSaxena2214 <148477562+MayankSaxena2214@users.noreply.github.com>
2024-02-14 23:07:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (24.14%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,39 +1,56 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> s={1};
-        vector<int> ans;
-        int carry=0;
-        int i=digits.size()-1;
-        int j=s.size()-1;
-        while(i>=0 && j>=0){
-            int sum=digits[i]+s[j]+carry;
-            carry=sum/10;
-            sum=sum%10;
-            ans.push_back(sum);
-            i--;
-            j--;
+        int n=digits.size();
+        for(int i=n-1;i>=0;i--){
+            if(i==n-1){
+                digits[i]++;
+            }
+            if(digits[i]==10){
+                digits[i]=0;
+                if(i!=0){
+                    digits[i-1]++;
+                }
+                else{
+                    digits.push_back(0);
+                    digits[i]=1;
+                }
+            }
         }
-        while(i>=0){
-            int sum=digits[i]+carry;
-            carry=sum/10;
-            sum=sum%10;
-            ans.push_back(sum);
-            i--;
-        }
+        return digits;
+        // vector<int> s={1};
+        // vector<int> ans;
+        // int carry=0;
+        // int i=digits.size()-1;
+        // int j=s.size()-1;
+        // while(i>=0 && j>=0){
+        //     int sum=digits[i]+s[j]+carry;
+        //     carry=sum/10;
+        //     sum=sum%10;
+        //     ans.push_back(sum);
+        //     i--;
+        //     j--;
+        // }
+        // while(i>=0){
+        //     int sum=digits[i]+carry;
+        //     carry=sum/10;
+        //     sum=sum%10;
+        //     ans.push_back(sum);
+        //     i--;
+        // }
         
-        while(carry!=0){
-            int sum=carry;
-            carry=sum/10;
-            sum=sum%10;
-            ans.push_back(sum);
+        // while(carry!=0){
+        //     int sum=carry;
+        //     carry=sum/10;
+        //     sum=sum%10;
+        //     ans.push_back(sum);
             
-        }
-         int st=0;
-        int e=ans.size()-1;
-        while(st<e){
-            swap(ans[st++],ans[e--]);
-        }
-        return ans;
+        // }
+        //  int st=0;
+        // int e=ans.size()-1;
+        // while(st<e){
+        //     swap(ans[st++],ans[e--]);
+        // }
+        // return ans;
     }
 };
