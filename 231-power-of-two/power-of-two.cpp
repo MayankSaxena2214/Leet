4208078f4beb9ca5045dacfa229a47522b8455e4
MayankSaxena2214 <148477562+MayankSaxena2214@users.noreply.github.com>
2024-02-19 10:54:58 +0530

    Time: 5 ms (20.09%) | Memory: 7 MB (49.03%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        // int ans=1;
+        // if(ans==n)
+        // return true;
+        // for(int i=0;i<30;i++){
+        //     ans=ans*2;
+        //     if(ans==n){
+        //         return true;
+        //     }
+        // }
+        // return false;
         int ans=1;
         if(ans==n)
-        return true;
+            return true;
         for(int i=0;i<30;i++){
-            ans=ans*2;
+            
+            if(ans<INT_MAX/2)
+                ans=ans*2;
             if(ans==n){
                 return true;
             }
+            // else
+            //     return false;
+            
         }
         return false;
     }
